710c24dfc559d91a1ef998c9a23de9dd3c574ea4
rubenalves-dev <hello@raiiaa.dev>
2026-02-11 09:03:55 +0000

    fix: correct TokenStore import in auth interceptors

--- client/projects/frontoffice/src/app/auth.interceptor.ts
+++ client/projects/frontoffice/src/app/auth.interceptor.ts
@@ -1,10 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 
-import { AuthTokenStoreService } from 'lib';
+import { TokenStore } from 'lib';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const tokenStore = inject(AuthTokenStoreService);
+  const tokenStore = inject(TokenStore);
   const accessToken = tokenStore.getAccessToken();
 
   if (!accessToken) {
